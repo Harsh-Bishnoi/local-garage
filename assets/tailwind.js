70052f21a4fcdffca28797abcafc7e0382784629
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-16 11:06:25 +0530

    price guide section done

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -3,8 +3,12 @@ tailwind.config = {
         extend: {
             colors: {
                 "light-black": "#1F2937",
-                
+                "light-gray": "#8080800D",
+                "dark-gray": "#969696",
+                "red": "#F94600",
+                "light-red": "#dc2626",
+                "light-green": "#F1F5F0"
             },
         },
     },
-  };
+};
