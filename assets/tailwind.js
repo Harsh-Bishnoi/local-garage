4e35e21541babcfcccd9a3a91229c37a166cec83
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-16 12:56:36 +0530

    available now section almost done

--- assets/tailwind.js
+++ assets/tailwind.js
@@ -3,11 +3,13 @@ tailwind.config = {
         extend: {
             colors: {
                 "light-black": "#1F2937",
-                "light-gray": "#8080800D",
+                "lighter-gray": "#8080800D",
                 "dark-gray": "#969696",
                 "red": "#F94600",
                 "light-red": "#dc2626",
-                "light-green": "#F1F5F0"
+                "light-green": "#F1F5F0",
+                "light-gray": "#F3F4F6",
+                "blue": "#2289e1"
             },
         },
     },
